1165eb27d5756eceaa450f594d05733711c23292
Raine <64136899+raineio@users.noreply.github.com>
2023-07-28 23:51:29 +0200

    ok but fr this time

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,11 @@
-import React from "react"
-
 function Navbar() {
     return(
     <>
         <div>
-            this is a navbar.
+            this is a navbar
         </div>
     </>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
