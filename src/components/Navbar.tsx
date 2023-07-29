7dcc3c5b8a4b835cf43b19ff2b15cd0a08341363
Raine <64136899+raineio@users.noreply.github.com>
2023-07-29 19:02:40 +0200

    update project slightly

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return (
         <>
-            <div className="static bg-slate-700">
+            <div className="stick w-screen">
                 <a href="">Leaderboards</a>
                 <a href="">Challenges</a>
                 <a href="">Shop</a>
