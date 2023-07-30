4fbe8278f584e438fba97c548b036bf7e6bef7cb
NightHawk <hasfeldt@yahoo.dk>
2023-07-30 22:49:53 +0200

    Revert path-aliasing

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.scss";
 import App from "./App";
-import Navbar from "@Navbar";
-import Footer from "@Footer";
-/* Could also be:
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-import Navbar from "@comps/Navbar";
-import Footer from "@comps/Footer";
-
-Because of the @comps/* alias
-
-*/
 
 ReactDOM.render(
   <React.StrictMode>
